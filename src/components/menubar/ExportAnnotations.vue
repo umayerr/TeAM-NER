f3016f9241ed571e343995ee775a17d57f3a4b53
umayerr <a.reza@maine.edu>
2024-09-22 21:20:51 -0400

    Format annotation time

--- src/components/menubar/ExportAnnotations.vue
+++ src/components/menubar/ExportAnnotations.vue
@@ -41,7 +41,7 @@ export default {
 
               const newHistoryEntry = [
                 state,
-                new Date().toLocaleString(),
+                this.formatDate(new Date()),
                 annotator,
                 entity[3], // The class or label from the entity
               ];
@@ -65,6 +65,17 @@ export default {
       } catch (error) {
         console.error("Export failed:", error);
       }
+    },
+	
+	formatDate(date) {
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+      const day = String(date.getDate()).padStart(2, '0');
+      const hours = String(date.getHours()).padStart(2, '0');
+      const minutes = String(date.getMinutes()).padStart(2, '0');
+      const seconds = String(date.getSeconds()).padStart(2, '0');
+
+      return `${year}-${month}-${day}~${hours}:${minutes}:${seconds}`;
     },
   },
 }
